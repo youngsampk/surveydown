5f0964d2851e2de35c1f3dd9ee3581a5c02afc81
youngsampk <youngsampk@gmail.com>
2025-08-03 18:42:04 +0900

    initial post

---
+++ surveydown_test.R
@@ -0,0 +1,21 @@
+library(surveydown)
+surveydown::sd_version()
+surveydown::sd_create_survey(
+  path = "d:/github_quarto/surveydown"
+)
+surveydown::sd_db_config()
+
+aws-0-ap-northeast-2.pooler.supabase.com
+6543
+postgres
+postgres.zxbhxuofxeosuolifwqd
+qlwjdrb21YS**
+ystable
+
+
+surveydown::sd_dashboard()
+
+sd_dashboard()
+install.packages("sdstudio")
+pak::pak("surveydown-dev/sdstudio", ask = FALSE)
+sdstudio::launch()
